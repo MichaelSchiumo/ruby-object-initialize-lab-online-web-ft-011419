6f6e38e8ea57718c9ca69446a8f5efc4a5e3a685
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-21 22:50:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,28 @@
 class Dog 
-  def initialize(dog_name)
-    @name = dog_name
+  def initialize(name)
+    @name = name
   end 
   
-  def name=(dog_name)
-    @name = dog_name
+  def name=(name)
+    @name = name
   end 
   
   def name
     @name
   end
-end 
+  
+  def initialize(breed)
+    @breed = breed
+  end 
+  
+  def breed=(breed)
+    @breed = breed 
+  end 
+  
+  def breed
+    @breed
+  end   
+end
 
 
 
